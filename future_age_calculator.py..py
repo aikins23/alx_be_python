16fb72ff6102901acb006f842dbc724f8ad88cef
aikins23 <buabengemmanuelaikins@gmail.com>
2025-09-12 12:20:14 +0000

    introduction to python mandatory projects

---
+++ future_age_calculator.py..py
@@ -0,0 +1,3 @@
+user_age = int(input("Enter your age: "))
+future_age = user_age + (2050 -2023)
+print(f"in 2050 you would be : {future_age} years old")
---
+++ hours_to_seconds.py
@@ -0,0 +1,4 @@
+Const_seconds = 3600
+hours = 2
+hours_seconds = hours * Const_seconds
+print(f"2 hour(s) is: {hours_seconds} seconds")
