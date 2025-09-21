3a849f4670093134a5b557a93af160d725c0cdf2
aikins23 <buabengemmanuelaikins@gmail.com>
2025-09-21 14:59:26 +0000

    ALX CONTROL FLOW

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,4 +1,4 @@
-task = input("Enter your task: Finish project report: ")
+task = input("Enter your task:  ")
 priority =input("Priority (high/medium/low): ")
 time_bound = input("Is it time-bound? (yes/no): ")
 
