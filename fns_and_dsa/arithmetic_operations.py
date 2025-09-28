f153f4af2150fc1e4f036ea8620b4d3e48d019a0
aikins23 <buabengemmanuelaikins@gmail.com>
2025-09-28 19:46:25 +0000

    0. Arithmetic Operations Function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,8 +6,8 @@ def perform_operation(num1, num2, operation):
     elif operation == 'multiply':
         return num1 * num2
     elif operation == 'divide':
-        if num2/0:
-            print("Division by zero not possible")
+        if num2 ==0:
+            return "Error: Division by zero"
         else:
             return num1 / num2
     else:
